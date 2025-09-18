88c2fc85d2ad678cac0d2c67de32c25f93492bfe
Estre25 <estrellamarc250@gmail.com>
2025-09-17 21:14:15 -0600

    Se creo la arrow fuction del inciso b

--- Practica1/a.js
+++ Practica1/a.js
@@ -4,3 +4,9 @@ let edad = 25;
 console.log (saludo = "Hola, " + nombre + ". Tienes " + edad + "años.");
 
 
+const cuadrado = (numero) => (numero * numero);
+console.log (cuadrado(4));
+console.log (cuadrado(10));
+console.log (cuadrado(80));
+
+
