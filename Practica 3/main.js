572bc9f72c8348f122b3d2d1ab73d2d0d83a721b
Estre25 <estrellamarc250@gmail.com>
2025-09-25 17:04:42 -0600

    Creación de retorno de una promesa

--- Practica 3/main.js
+++ Practica 3/main.js
@@ -4,3 +4,23 @@ console.log("10 - 4 = ", restar(10, 4));
 console.log("5 - 8 = ", restar(5, 8));
 console.log("6 - 3 = ",restar(6, 3));
 
+function verificarUsuario(usuario){
+      return new Promise((resolve, reject) =>
+      {
+        if (usuario === "admin")
+        {
+            resolve ("Acceso concedido");
+        }else
+        {
+            reject("Acceso denegado");
+        }
+      }
+    );
+}
+verificarUsuario("admin")
+.then(res => console.log(res))
+.catch(err => console.error(err));
+
+verificarUsuario("Ivan")
+.then(res => console.log(res))
+.catch(err => console.error(err));
